ee86cd2c6b5d8e28639af7caf6e4c33325c25e26
kevinsun127 <kevinsun@usc.edu>
2024-02-26 20:45:11 -0500

    labels

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
-import { AccessTime, DeleteOutline, Download } from "@mui/icons-material";
+import {
+  AccessTime,
+  Delete,
+  DeleteOutline,
+  Download,
+} from "@mui/icons-material";
 import { useEffect, useState } from "react";
 
 import Head from "next/head";
@@ -9,14 +14,19 @@ import { UploadFileButton } from "~/components/fileField";
 type TimeLog = {
   startTime: Date;
   endTime: Date;
+  labels?: string[];
   notes?: string;
 };
 
 export default function Home() {
   const [timerStartTime, setTimerStartTime] = useState<Date>();
   const [currentTime, setCurrentTime] = useState<Date>(new Date());
+
   const [newLog, setNewLog] = useState<TimeLog>();
+  const [newLabel, setNewLabel] = useState<string>();
+
   const [logs, setLogs] = useState<TimeLog[]>();
+  const [labels, setLabels] = useState<string[]>();
 
   useEffect(() => {
     const timer = setInterval(() => setCurrentTime(new Date()), 100);
@@ -24,6 +34,27 @@ export default function Home() {
       clearInterval(timer);
     };
   }, []);
+
+  useEffect(() => {
+    if (labels == undefined) {
+      try {
+        const items = localStorage.getItem("labels");
+        if (!items) setLabels([]);
+        else {
+          const newLabels = JSON.parse(items) as string[];
+          setLabels(newLabels ?? []);
+        }
+      } catch (e) {
+        setLabels([]);
+      }
+    } else {
+      try {
+        localStorage.setItem("labels", JSON.stringify(labels));
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }, [labels]);
 
   useEffect(() => {
     if (logs == undefined) {
@@ -192,6 +223,48 @@ export default function Home() {
                       }
                     />
                   </p>
+                  <p className="flex flex-col gap-2">
+                    <h4 className="text-xs font-medium">label</h4>
+                    <div className="flex flex-col gap-4">
+                      <select
+                        className="w-fit rounded-md px-2 py-1 text-xs"
+                        onChange={(e) => {
+                          console.log(e.currentTarget.value);
+                          setNewLog({
+                            ...newLog,
+                            labels: (newLog.labels ?? []).concat(
+                              e.currentTarget.value,
+                            ),
+                          });
+                        }}
+                      >
+                        <option>---</option>
+                        {labels?.map((label) => (
+                          <option key={label}>{label}</option>
+                        ))}
+                      </select>
+                      <div className="flex flex-row items-center gap-2">
+                        {newLog.labels?.map((label) => (
+                          <div
+                            key={label}
+                            className="flex flex-row items-center gap-2 rounded-md bg-slate-300 p-2 text-xs"
+                          >
+                            <span>{label}</span>
+                            <button
+                              onClick={() =>
+                                setNewLog({
+                                  ...newLog,
+                                  labels: labels?.filter((l) => l !== label),
+                                })
+                              }
+                            >
+                              <Delete fontSize="small" />
+                            </button>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </p>
                   <StyledButton
                     onClick={() => {
                       setLogs((logs) => (logs ?? []).concat(newLog));
@@ -205,6 +278,44 @@ export default function Home() {
             )}
           </StyledContainer>
         </div>
+        <StyledContainer>
+          <h2 className="text-sm font-medium">labels</h2>
+          <div className="mb-2 flex flex-row gap-2">
+            {labels?.map((label) => (
+              <div
+                key={label}
+                className="flex flex-row items-center gap-2 rounded-md bg-slate-300 p-2 text-xs"
+              >
+                <span>{label}</span>
+                <button
+                  onClick={() =>
+                    setLabels((labels) => labels?.filter((l) => l !== label))
+                  }
+                >
+                  <Delete fontSize="small" />
+                </button>
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-row gap-2">
+            <input
+              className="rounded-md p-2 text-xs"
+              value={newLabel}
+              onChange={(e) => setNewLabel(e.target.value)}
+            />
+            <StyledButton
+              className="text-xs"
+              onClick={() => {
+                if (newLabel && newLabel.length > 0) {
+                  setLabels((labels) => (labels ?? []).concat(newLabel));
+                  setNewLabel("");
+                }
+              }}
+            >
+              Create
+            </StyledButton>
+          </div>
+        </StyledContainer>
         <StyledContainer>
           <h2 className="text-sm font-medium">time today</h2>
           <p className="text-md font-semibold">
@@ -280,7 +391,7 @@ export default function Home() {
                       .map((log, i) => (
                         <li
                           key={i}
-                          className="flex flex-col gap-1 rounded-md bg-slate-300/70 p-2 text-xs"
+                          className="flex flex-col gap-2 rounded-md bg-slate-300/70 p-2 text-xs"
                         >
                           <div className="flex flex-row items-center gap-2">
                             <p className="text-sm font-medium">
@@ -302,6 +413,18 @@ export default function Home() {
                             </p>
                           </div>
                           {log.notes && <p>{log.notes}</p>}
+                          {log.labels && (
+                            <div className="flex flex-row items-center gap-2">
+                              {log.labels.map((label, i) => (
+                                <div
+                                  key={i}
+                                  className="rounded-md border border-slate-700 px-2 py-1 text-slate-700"
+                                >
+                                  {label}
+                                </div>
+                              ))}
+                            </div>
+                          )}
                         </li>
                       ))}
                   </ul>
